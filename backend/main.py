32c740f0964d7d6122195ecf46fef21d770e97b7
Sandeep Mishra <sandeep2002mishra@tamu.edu>
2025-08-07 15:46:41 -0500

    Fixed CORS issue

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import ai
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_origins=[
+        "https://ai-job-coach-eight.vercel.app",  
+        "http://localhost:5173"  # local testing
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(ai.router)
+
 
